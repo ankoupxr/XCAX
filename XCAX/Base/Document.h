b2a5718250c4437721659fd851c483193832b13b
ankoupxr <43463848+ankoupxr@users.noreply.github.com>
2023-10-08 01:07:39 +0800

    修改

--- XCAX/Base/Document.h
+++ XCAX/Base/Document.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <TDocStd_Document.hxx>
-
+#include <TreeNode.h>
 
 class Document;
 DEFINE_STANDARD_HANDLE(Document, TDocStd_Document)
@@ -10,14 +10,17 @@ using DocPtr = Handle(Document);
 class  Document : public TDocStd_Document
 {
 public:
+	int Identify() const { return m_identify; };
 
 	
 	TDF_Label RootLabel() const;
 	TDF_Label EntityLable() const;
 
+	bool IsEntity(TreeNodeId nodeId);
+	int EntityCount() const;
 
 private:
-
+	int m_identify;
 };
 
 

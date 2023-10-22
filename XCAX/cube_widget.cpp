ec0cc56c221909c4c3f58440c53f460dd4fae992
ankoupxr <43463848+ankoupxr@users.noreply.github.com>
2023-10-23 00:16:50 +0800

    1

--- XCAX/cube_widget.cpp
+++ XCAX/cube_widget.cpp
@@ -1,1 +1,24 @@
 #include "cube_widget.h"
+
+cube_widget::cube_widget(AppPtr app):  m_app(app)
+{
+	m_ui.setupUi(this);
+	//QObject::connect(this,&QTreeWidget::itemSelectionChanged,this, &ModelTreeWidget::sendModelTreeItemSignalBuilder);
+	QObject::connect(m_ui.confirm, SIGNAL(clicked()), this, SLOT(OnConfirm()));
+}
+
+cube_widget::~cube_widget()
+{}
+
+void cube_widget::OnConfirm() 
+{
+	gp_Pnt P(m_ui.coordX->text().toDouble(), m_ui.coordY->text().toDouble(), m_ui.coordZ->text().toDouble());
+	TopoDS_Shape cube = BRepPrimAPI_MakeBox(P, m_ui.cubesizeX->text().toDouble(), m_ui.cubesizeY->text().toDouble(),
+		m_ui.cubesizeZ->text().toDouble()).Shape();
+	m_app->GetMainWin()->renderShape(cube);
+}
+
+void cube_widget::OnCancel() 
+{
+
+}
